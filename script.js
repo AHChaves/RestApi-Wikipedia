b6eefd37e4f7bd60092e5bf74367b73961770e04
hashi012 <arthurhenri2006@gmail.com>
2023-11-23 11:06:11 -0300

    parte 2 css

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelector(`.search-box`);
+    const input = form.querySelector(`input[type="search"]`);
+
+    
+})

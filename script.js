97a99a18b7e19a484a556c0ad64a85e4bcc8bc16
Eduardo Birchal <edubirchal@gmail.com>
2023-11-24 00:56:21 -0300

    Pega informações sobre a página
    Agora só falta colocar elas de forma bonitinha

--- script.js
+++ script.js
@@ -25,14 +25,34 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
         resultsContainer.innerHTML = '';
-        resultsCounter.textContent = `Numero de resultados: ${results.length}`
+        resultsCounter.textContent = `Número de resultados: ${results.length}`
+        
         results.forEach(result => {
-            console.log(result.snippet)
+            //console.log(result);
             const resultsElements = document.createElement('div');
             resultsElements.className = 'result';
             resultsElements.innerHTML = `<h3>${result.title}</h3><p>${result.snippet}</p>`;
             resultsContainer.appendChild(resultsElements);
+
+            getDetails(result);
         });
     }
 
+    function getDetails(result) {
+        const pageUrl = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&redirects=1&origin=*&pageids=${result.pageid}`;
+
+        fetch(pageUrl).then(response => response.json()).then(data => {
+            let pageList = data.query.pages;
+            Object.keys(pageList).forEach(id => {
+                printDetails(pageList[id]);
+            })
+        }).catch(error => alert('Error: ' + error));
+    }
+
+    function printDetails(page) {
+        const pageDetails = document.createElement('div');
+        //resultsElements.className = 'result';
+        pageDetails.innerHTML = `<p>titulo: ${page.title}</p>`;
+        resultsContainer.appendChild(pageDetails);
+    }
 });

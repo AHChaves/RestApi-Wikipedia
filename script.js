6274801422529b8c8b3f1c3388760d0977e07455
Eduardo Birchal <edubirchal@gmail.com>
2023-11-24 21:58:05 -0300

    ADD: Coisas

--- script.js
+++ script.js
@@ -4,13 +4,31 @@ document.addEventListener('DOMContentLoaded', function(){
     const resultsContainer = document.querySelector('.results');
     const resultsCounter = document.querySelector('header p');
 
+    setupModal();
+
     form.addEventListener('submit', function(event){
         event.preventDefault();
         const searchTerm = input.value;
+
         if(searchTerm){
             searchWikipedia(searchTerm);
         }
     });
+
+    function setupModal() {
+        const modal = document.getElementById("modalTextoWikipedia");
+        const span = document.getElementsByClassName("fecharModal")[0];
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
 
     function searchWikipedia(searchTerm){
         const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(searchTerm)}`;
@@ -22,20 +40,49 @@ document.addEventListener('DOMContentLoaded', function(){
 
     
     function displayResults(results){
-
-
         resultsContainer.innerHTML = '';
         resultsCounter.textContent = `Número de resultados: ${results.length}`
         
         results.forEach(result => {
-            //console.log(result);
             const resultsElements = document.createElement('div');
+
+            let titulo = document.createElement('a');
+
+            titulo.setAttribute('href', '#');
+            titulo.setAttribute('id', `titulo${result.pageid}`);
+            titulo.innerHTML = result.title;
+
+            console.log(titulo);
+            titulo.addEventListener("click", function () { console.log("bababa"); });
+
+            resultsElements.appendChild(titulo);
             resultsElements.className = 'result';
-            resultsElements.innerHTML = `<a href="#" if="titulo${result.pageid}">${result.title}</a><p>${result.snippet}</p>`;
+            resultsElements.innerHTML += `<p>${result.snippet}</p>`;
+
             resultsContainer.appendChild(resultsElements);
 
             printDetails(result, resultsElements);
         });
+    }
+
+    function openModal() {
+        console.log("babababab");
+
+        modal.style.display = "block";
+        changeModalText();
+    }
+
+    function changeModalText() {
+        fetch(pageUrl).then(response => response.json()).then(data => {
+            const pageList = data.query.pages;
+
+            Object.keys(pageList).forEach(id => {
+                const currentPage = pageList[id];
+                const conteudoModal = document.getElementById("conteudoPagina");
+
+                conteudoModal.innerHTML = currentPage.extract;
+            })
+        }).catch(error => alert('Error: ' + error));
     }
 
     function printDetails(result, div) {
@@ -48,16 +95,6 @@ document.addEventListener('DOMContentLoaded', function(){
         createDetailBox(pageDetailsDiv, 'Núm. palavras', result.wordcount);
         createDetailBox(pageDetailsDiv, 'Tamanho (bytes)', result.size);
         createDetailBox(pageDetailsDiv, 'ID da página', result.pageid);
-
-        fetch(pageUrl).then(response => response.json()).then(data => {
-            const pageList = data.query.pages;
-
-            Object.keys(pageList).forEach(id => {
-                const currentPage = pageList[id];
-
-                
-            })
-        }).catch(error => alert('Error: ' + error));
     }
 
     function createDetailBox(div, string, value) {
